b6bf8f7a15809134e02e95f20911464caa9e6bb3
rk <srkv9192@gmail.com>
2023-05-14 13:37:37 +0530

    Add menu bar,newcasehtml and login html

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser')
         app.use(bodyParser.urlencoded({ extended: false }))
         app.use(bodyParser.json())
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => res.sendfile(__dirname+'/index.html'))
+
+app.get('/login.html', (req, res) => res.sendfile(__dirname+'/login.html'))
+app.get('/newcase.html', (req, res) => res.sendfile(__dirname+'/newcase.html'))
+app.get('/viewdata.html', (req, res) => res.sendfile(__dirname+'/viewdata.html'))
 
 app.listen(port, () => console.log(`Insurance app listening on port ${port}!`))
