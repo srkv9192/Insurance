9faa636a71f2216565f66ead4cb4556ac2aa5e49
rajat k <rajat@rajat.com>
2023-07-24 03:03:28 -0700

    point to correct db

--- server.js
+++ server.js
@@ -50,22 +50,20 @@ const port = process.env.PORT || 80
 //});
 
 
-/*
-
 mongoose.connect(`mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@cluster0.rldiof1.mongodb.net/nidaandatabase?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-*/
 
+/*
 
 mongoose.connect(`mongodb://127.0.0.1:27017/test`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
+*/
 
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
