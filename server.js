8352ab47b9cd20f14f050a551d9d4d461ece1414
rajat k <rajat@rajat.com>
2023-07-24 03:02:12 -0700

    New card template and card rules

--- server.js
+++ server.js
@@ -50,22 +50,22 @@ const port = process.env.PORT || 80
 //});
 
 
-
+/*
 
 mongoose.connect(`mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@cluster0.rldiof1.mongodb.net/nidaandatabase?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+*/
 
-/*
 
 mongoose.connect(`mongodb://127.0.0.1:27017/test`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-*/
+
 
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
@@ -1662,7 +1662,7 @@ whereof I/We hereto at Indore signed on the ${agreementdate.getDate().toString()
 
 
 async function downloadCardPDF(req) {
-  const document = await PDFDocument.load(readFileSync("./cardtemplate3.pdf"));
+  const document = await PDFDocument.load(readFileSync("./cardtemplate4.pdf"));
 
   const courierBoldFont = await document.embedFont(StandardFonts.Courier);
   const timesBoldFont = await document.embedFont(StandardFonts.TimesRomanBold);
